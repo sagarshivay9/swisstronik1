9f39ea7d4b8f97a015eb6944872ac598b774c12b
sagar shivay <sagarshivay@gmail.com>
2024-07-31 07:53:05 +0000

    feat: initiated the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,15 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
-
-  await contract.waitForDeployment();
-
-  console.log(`This is your deployed contract address ${contract.target}`);
+  const perc20 = await ethers.deployContract("PERC20Sample");
+  await perc20.waitForDeployment();
+  const deployedContract = await perc20.getAddress();
+  fs.writeFileSync("contract.txt", deployedContract);
+  
+  console.log(`PERC20Sample was deployed to: ${deployedContract}`)
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
